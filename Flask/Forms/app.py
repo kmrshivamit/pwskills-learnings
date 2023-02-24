4d697f753c6c8e3711a2317b947dd5796203a112
Kumar Shivam <kmrshivamit@gmail.com>
2023-02-24 13:27:57 +0530

    adding for flask and web scraping

---
+++ Flask/Forms/app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template,request,jsonify
+
+app=Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def home_page():
+    return render_template('index.html')
+@app.route('/math',methods=['POST'])
+def math_operation():
+    if(request.method=='POST'):
+        ops=request.form['operation']
+        num1=int(request.form['num1'])
+        num2=int(request.form['num2'])
+        if(ops=='add'):
+            result=num1+num2
+            result= f'the {ops} of {str(num1)} and {str(num2)} is str{str(result)}'
+
+        if(ops=='divide'):
+            result=num1/num2
+            result= f'the {ops} of {str(num1)} and {str(num2)} is str{str(result)}'
+    
+        if(ops=='subtract'):
+            result=num1-num2
+            result= f'the {ops} of {str(num1)} and {str(num2)} is str{str(result)}'
+
+        if(ops=='multiply'):
+            result=num1*num2
+            result= f'the {ops} of {str(num1)} and {str(num2)} is {str(result)}'
+
+        return render_template('results.html',result=result)
+    
+
+
+@app.route('/postman_data',methods=['POST'])
+def math_operation1():
+    if(request.method=='POST'):
+        ops=request.json['operation']
+        num1=int(request.json['num1'])
+        num2=int(request.json['num2'])
+        if(ops=='add'):
+            result=num1+num2
+            result= f'the {ops} of {str(num1)} and {str(num2)} is str{str(result)}'
+
+        if(ops=='divide'):
+            result=num1/num2
+            result= f'the {ops} of {str(num1)} and {str(num2)} is str{str(result)}'
+    
+        if(ops=='subtract'):
+            result=num1-num2
+            result= f'the {ops} of {str(num1)} and {str(num2)} is str{str(result)}'
+
+        if(ops=='multiply'):
+            result=num1*num2
+            result= f'the {ops} of {str(num1)} and {str(num2)} is {str(result)}'
+
+        return jsonify(result)
+    
+    
+        
+
+
+
+def home_page():
+    return render_template('index.html')
+
+@app.route('/')
+def hello_world():
+    return "<h1> Hello, World </h1"
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',debug=True)
